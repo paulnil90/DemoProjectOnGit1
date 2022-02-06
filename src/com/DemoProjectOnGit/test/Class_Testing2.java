db62328f9bfba44e25ffe09625b4c4c17095dcf0
user <paulniladri90@gmail.com>
2022-02-06 16:10:37 +0530

    Third print statement.

--- src/com/DemoProjectOnGit/test/Class_Testing2.java
+++ src/com/DemoProjectOnGit/test/Class_Testing2.java
@@ -5,8 +5,10 @@ public class Class_Testing2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Testing 2 Class");
+		System.out.println("Another Class");
 
+
+		System.out.println("Testing 2 Class");
 
 	}
 
