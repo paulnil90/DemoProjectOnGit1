a72c321431420603e5eb71792e2f6086318e0b8d
user <paulniladri90@gmail.com>
2022-02-06 12:22:05 +0530

    First commit

---
+++ src/com/DemoProjectOnGit/test/Class_Testing.java
@@ -0,0 +1,12 @@
+package com.DemoProjectOnGit.test;
+
+public class Class_Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Testing Class");
+
+	}
+
+}
